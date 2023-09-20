7884f1c21a38b918eaf8e78b79252f6fef6d6755
Nichell Gallenero <nichell.gallenero@students.isatu.edu.ph>
2023-09-20 03:59:42 +0000

    initial commit

---
+++ RWorksheet_Gallenero#1.R
@@ -0,0 +1,27 @@
+#Vector
+
+age <- c (34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 
+          27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 
+          17, 37, 42, 53, 41, 51, 35, 24, 33, 41)
+
+#1 Data points
+  length (age)
+  
+#2 Reciprocal
+  reciprocalAge <-1/age
+  reciprocalAge
+  
+#3 Assign also new age <- c(age, 0, age)
+  NewAge<-c (age, 0, age)
+  NewAge
+
+#4 Sort the values for age
+  SortValueAge<- sort(age)
+  SortValueAge
+
+#5 minimum and maximum age
+  minimum_Age<-min(age)
+  minimum_Age
+  maximum_Age<-max(age)
+  maximum_Age
+
